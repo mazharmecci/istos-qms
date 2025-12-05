4c89beddffad663455d2abe1db7d144596f7d7c3
openvault <157576678+mazharmecci@users.noreply.github.com>
2025-12-05 11:28:01 +0530

    Update db.js

---
+++ backend/src/config/db.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+host: process.env.DB_HOST || 'db',
+dialect: 'postgres',
+port: process.env.DB_PORT || 5432,
+logging: false,
+});
+
+
+module.exports = sequelize;
